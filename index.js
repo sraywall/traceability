ea707fc989d1fdab1ae9149b149cd7d2ae8f0152
Stephen Wall <sraywall@gmail.com>
2021-12-16 13:57:43 -0700

    changed html file

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const path = require('path')
 
 const app = express()
 app.use(express.json())
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    // record a generic message and send it to Rollbar
+    // rollbar.info("html file served successfully");
 
+})
 const port = process.env.PORT || 4545
 app.listen(port,()=>console.log(`Take us to warp ${port}`))
